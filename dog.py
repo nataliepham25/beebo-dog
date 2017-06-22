5a6323a795a5dfd5a6c0db4baea7bcdaff011203
Natalie Pham <nataliepham525@gmail.com>
2017-06-22 10:03:44 -0700

    Add files via upload

---
+++ dog.py
@@ -0,0 +1,78 @@
+start = '''
+One day a dog named Beebo ran away from his home in New York city. He
+felt lonely and wanted to explore the busy and captivating city. Help Beebo find
+his way home! HUEHUEHUEHUEHE
+'''
+# hallo my name is natalie and i like to eat ice cream
+#trolling
+print(start)
+# ask if the dog wants to go left or right
+print("Type 'left' to go left or 'right' to go right.")
+user_input = input()
+if user_input == "left":
+    print()
+    print("Beebo goes left and ends up at the pretty Central Park")
+    print()
+    print("Beebo encounters his FRIENDS at the Central Perk where they go to come to steal coffee and food from the humans.")
+    print("Beebo sees a bacon filled sandwich that he desires")
+    print("Decide what Beebo should do by typing 'Steal' to take the sandwich or 'run' to go to his friends")
+    user_input = input()
+    if user_input == "Steal"
+        print("Beebo hits his head on the wall")
+        print("Beebo walks out of the coffee shop to find his friends")
+    elif user_input == "run"
+        print("Good Job Beebo! Now go find your friends!")
+        print("Beebo walks through central park")
+    print("Beebo sees a squirrel... Type 'bark' or 'attack' to decide what he does next") # finished the story by writing what happens
+    user_input = input()
+    if user_input == "bark":
+        print()
+        print("Beebo's owner sees him and takes him home!")
+        print()
+        print("Yay! He found his home!")
+    elif user_input == "attack":
+        print()
+        print("Oh no! He was sent to the dog pound")
+        print()
+        print("Type 'wait' or 'escape' to find out what happens next!")
+        user_input = input()
+        if user_input == "wait":
+            print()
+            print("Beebo dies")
+        elif user_input == "escape":
+            print()
+            print("He reaches home")
+            print()
+            print("End Story.")
+        else:
+            print("Wrong Input")
+#YAS BEEBO YOU DID IT
+elif user_input == "right":
+    print("Beebo goes right and ends up in the sketchy streets of New York") # finished the story writing what happens
+    print()
+    print("Beebo sees a woman attacked by a criminal")
+    print()
+    print("Type 'fight' or 'walk' to to find out what happens next!")
+    user_input = input()
+    if user_input == "fight":
+        print()
+        print("Uh oh! Beebo got sent to the dog pound!")
+        print()
+        print("Type 'wait' or 'escape' to find out what happens next!")
+        user_input = input()
+        if user_input == "wait":
+            print()
+            print("Beebo dies")
+        elif user_input == "escape":
+            print()
+            print("He reaches home")
+            print()
+            print("End Story.")
+        else:
+            print("Wrong Input!")
+    elif user_input == "walk":
+        print("Beebo's owner sees him and takes him home!")
+        print()
+        print("Yay! He found his home!")
+    else:
+        print("Wrong Input!")
